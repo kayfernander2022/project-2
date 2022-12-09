597c46779c482475c3de503b8f147cb0b838821f
kayfernander2022 <kayfernander@hotmail.com>
2022-12-09 10:25:45 -0500

    working routes

--- server.js
+++ server.js
@@ -20,20 +20,93 @@ app.use("/static",express.static("public"))// /static is needed here to show pat
 app.use(express.urlencoded({extended: true})) 
 
 //Routes and Routers
+
+//Home
 app.get("/",(req,res) => {
   //res.send("<h1>Hey! Server is Working</h1>")
-  res.render("index.ejs")
+  res.render("index.ejs")//landing ejs file
 })
 
-app.get("/recipes",(req,res) => {
+//New
+app.get("/recipes/new", (req, res) => {
+  res.render("./recipes/new.ejs");
+});
+
+//User
+app.get("/user/login",(req,res) => {
+  //res.send("<h1>Hey! Server is Working</h1>")
+  res.render("./user/login.ejs")//landing ejs file
+})
+
+app.get("/user/signup",(req,res) => {
+  //res.send("<h1>Hey! Server is Working</h1>")
+  res.render("./user/signup.ejs")//landing ejs file
+})
+
+//Index
+app.get("/recipes/:userId",(req,res) => {
   recipe.find({}, (err, recipe) => {
   res.render("./recipes/index.ejs",{recipe})
 });
 });
 
+app.get("/recipes/viewall",(req,res) => {
+  recipe.find({}, (err, recipe) => {
+  res.render("./recipes/index.ejs",{recipe})
+});
+});
 
 //show
+app.get('/recipes/:id/show', (req, res) => {
+  res.render('./recipes/show.ejs', { recipe: recipe[req.params.id], index: req.params.id });//giving the show one poke
+  });
 
+app.post("/user/login", (req, res) => {
+    //need to add logic to check if user exist in mongo. 
+    // if they do redict to the users recipe/index
+    // if they do not show error and request user to signup ????
+  });
+
+  app.post("/user/signup", (req, res) => {
+    //check if user exist in mongo 
+    //if they do not create user in mongo
+    //if they do show user already exist
+    //redirect to login page
+  });
+
+app.post("/recipes", (req, res) => {
+  recipe.create(req.body,(err,fruit) =>{
+  res.redirect("./recipes/index.ejs")
+    });
+  });
+
+//Recipes
+//Delete 
+app.delete("/recipes/:id", (req, res) => {
+  const id = req.params.id
+  recipe.findByIdAndDelete(id, (err, recipe) => {
+      
+    res.redirect("./recipes/index.ejs")
+})
+})
+
+//Create
+app.post("/recipes", (req, res) => {
+recipe.create(req.body,(err,fruit) =>{
+res.redirect("./recipes/index.ejs")
+  });
+});
+
+//EDIT 
+app.get("/recipes/:id/edit", (req, res) => {
+  const id = req.params.id
+  
+  recipe.findById(id, (err, recipe) => {
+    console.log(err);
+      
+      res.render("./recipes/edit.ejs", {recipe})
+  })
+})
 
 //start server
 const PORT =process.env.PORT || 3000
